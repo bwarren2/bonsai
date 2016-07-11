4f8ee4218e9ca4d998e35f322e77d1f9c6db2321
Kit La Touche <kit@transneptune.net>
2016-07-11 09:27:51 -0600

    Add basic Plan interface.

--- app/controllers/plan.js
+++ app/controllers/plan.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  highlightedTask: null,
+
+  actions: {
+    addBlocks: function (task) {
+      const highlightedTask = this.get('highlightedTask');
+      if (!highlightedTask) {
+        this.set('highlightedTask', task);
+      } else {
+        this.get('highlightedTask').addBlocks(task);
+        this.set('highlightedTask', null);
+      }
+    },
+    clearBlocks: function (task) {
+      task.clearBlocks();
+    }
+  }
 });
--- app/models/task.js
+++ app/models/task.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import { belongsTo, hasMany } from 'ember-data/relationships';
@@ -6,5 +7,27 @@ export default Model.extend({
   title: attr('string'),
   completed: attr('boolean'),
   blocks: hasMany('task', { inverse: 'blockers' }),
-  blockers: belongsTo('task', { inverse: 'blocks' })
+  blockers: belongsTo('task', { inverse: 'blocks' }),
+
+  blocksCount: Ember.computed('blocks', function () {
+    return this.get('blocks').length;
+  }),
+
+  blockersCount: Ember.computed('blockers', function () {
+    return this.get('blockers').length;
+  }),
+
+  addBlocks: function (task) {
+    task.set('blockers', this);
+    this.get('blocks').pushObject(task);
+  },
+
+  clearBlocks: function () {
+    this.get('blocks').then((blocks) => {
+      blocks.forEach((block) => {
+        block.set('blockers', null);
+      });
+      this.get('blocks').removeObjects(blocks);
+    });
+  }
 });

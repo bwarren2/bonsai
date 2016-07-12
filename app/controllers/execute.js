0d1fde86f4283eed2347c2024f7a57232b17da6e
Kit La Touche <kit@transneptune.net>
2016-07-11 23:40:33 -0600

    Add basic execute interface.

--- app/controllers/execute.js
+++ app/controllers/execute.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    complete: function (task) {
+      task.set('completed', true);
+      task.save();
+      // TODO: Take this task off the top, reveal the next one.
+    }
+  }
 });

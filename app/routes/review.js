5b1e09e0726cd2c1ead59276b02f65ea81e6b801
Kit La Touche <kit@transneptune.net>
2016-07-11 23:58:05 -0600

    Basic review interface.

--- app/routes/review.js
+++ app/routes/review.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model () {
-    return this.store.findAll('task');
+    return this.store.findAll('task', {
+      completed: true
+    });
   }
 });

bd99277bd33c15b96a257fb49fe5a7b8a286b46d
Kit La Touche <kit@transneptune.net>
2016-07-14 14:14:10 -0600

    Show graph of planning dependencies.

--- app/components/task-graph.js
+++ app/components/task-graph.js
@@ -1,5 +1,7 @@
 import d3 from 'd3';
 import Ember from 'ember';
+import _ from 'lodash/lodash';
+const { RSVP } = Ember;
 
 export default Ember.Component.extend({
   didRender () {
@@ -15,55 +17,74 @@ export default Ember.Component.extend({
     vis.attr("width", w)
       .attr("height", h);
 
-    // TODO inject this:
-    const nodes = this.get('nodes').map(
-      (task) => ({
-        x: Math.random() * w,
-        y: Math.random() * h
-      })
+    // Mutate data for just the moment:
+    // We need to put the data on the objects so that the call to .afters below
+    // will pick it up correctly.
+    this.get('data').forEach(
+      (task) => {
+        task.x = Math.random() * w;
+        task.y = Math.random() * h;
+      }
     );
 
-    const links = [];
-      // {source: nodes[0], target: nodes[1]},
-      // {source: nodes[2], target: nodes[1]}
-    // ];
+    RSVP.all(this.get('data').map((task) => {
+      return task.get('afters').then((afters) => afters.map(
+        (after) => ({
+          source: {
+            x: task.x,
+            y: task.y
+          },
+          target: {
+            x: after.x,
+            y: after.y
+          }
+        })
+      ));
+    })).then((resolvedEdges) => {
+      const nodes = this.get('data').map(
+        (task) => ({
+          x: task.x,
+          y: task.y
+        })
+      );
+      const edges = _.flatten(resolvedEdges);
+      // Set up svg elements:
+      vis.append("defs")
+        .append("marker")
+        .attr("id", "arrow")
+        .attr("viewBox", "0 0 10 10")
+        .attr("refX", 1)
+        .attr("refY", 5)
+        .attr("markerWidth", 6)
+        .attr("markerHeight", 6)
+        .attr("orient", "auto")
+        .append("path")
+        .attr("d", "M 0 0 L 10 5 L 0 10 z")
+        .attr("fill", "gray");
 
-    // Set up svg elements:
-    vis.append("defs")
-      .append("marker")
-      .attr("id", "arrow")
-      .attr("viewBox", "0 0 10 10")
-      .attr("refX", 1)
-      .attr("refY", 5)
-      .attr("markerWidth", 6)
-      .attr("markerHeight", 6)
-      .attr("orient", "auto")
-      .append("path")
-      .attr("d", "M 0 0 L 10 5 L 0 10 z")
-      .attr("fill", "gray");
+      vis.selectAll("circle .nodes")
+        .data(nodes)
+        .enter()
+        .append("circle")
+        .attr("class", "nodes")
+        .attr("cx", (d) => d.x)
+        .attr("cy", (d) => d.y)
+        .attr("r", "5px")
+        .attr("fill", "black");
 
-    vis.selectAll("circle .nodes")
-      .data(nodes)
-      .enter()
-      .append("circle")
-      .attr("class", "nodes")
-      .attr("cx", (d) => d.x)
-      .attr("cy", (d) => d.y)
-      .attr("r", "5px")
-      .attr("fill", "black");
-
-    vis.selectAll(".line")
-      .data(links)
-      .enter()
-      .append("line")
+      vis.selectAll(".line")
+        .data(edges)
+        .enter()
+        .append("line")
       // TODO offset these by the length of the arrow + the radius of the circle.
-      .attr("x1", (d) => d.source.x)
-      .attr("y1", (d) => d.source.y)
-      .attr("x2", (d) => d.target.x)
-      .attr("y2", (d) => d.target.y)
-      .attr("fill", "none")
-      .attr("stroke", "gray")
-      .attr("stroke-width", 2)
-      .attr("marker-end", `url(${currentPath}#arrow)`);
+        .attr("x1", (d) => d.source.x)
+        .attr("y1", (d) => d.source.y)
+        .attr("x2", (d) => d.target.x)
+        .attr("y2", (d) => d.target.y)
+        .attr("fill", "none")
+        .attr("stroke", "gray")
+        .attr("stroke-width", 2)
+        .attr("marker-end", `url(${currentPath}#arrow)`);
+    });
   }
 });

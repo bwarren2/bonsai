cc0a42a3385e99ea686af621ceaea952d0c35af5
Kit La Touche <kit@transneptune.net>
2016-07-19 19:59:17 -0600

    Prevent adding blank tasks.

--- app/controllers/brainstorm.js
+++ app/controllers/brainstorm.js
@@ -10,7 +10,9 @@ export default Ember.Controller.extend({
     addTask () {
       const title = this.get('newTaskTitle');
       this.set('newTaskTitle', '');
-      this.store.createRecord('task', { title }).save();
+      if (Boolean(title)) {
+        this.store.createRecord('task', { title }).save();
+      }
     }
   }
 });

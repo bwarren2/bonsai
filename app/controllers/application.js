ef737a802fa9e154ca851139eb427c0b88f09a67
Kit La Touche <kit@transneptune.net>
2016-08-01 15:27:21 -0600

    Add basic keyboard commands.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,7 +1,31 @@
 import Ember from 'ember';
+import { EKMixin } from 'ember-keyboard';
+import { keyDown } from 'ember-keyboard';
 
-export default Ember.Controller.extend({
+export default Ember.Controller.extend(EKMixin, {
   session: Ember.inject.service('session'),
+  helpShowing: false,
+
+  activateKeyboard: Ember.on('init', function () {
+    console.log('activating keyboard');
+    this.set('keyboardActivated', true);
+  }),
+
+  toggleHelp: Ember.on(keyDown('shift+Slash'), function () {
+    if (this.get('helpShowing')) {
+      this.hideHelp();
+    } else {
+      this.showHelp();
+    }
+  }),
+
+  showHelp () {
+    this.set('helpShowing', true);
+  },
+
+  hideHelp () {
+    this.set('helpShowing', false);
+  },
 
   actions: {
     invalidateSession() {

827b660bce1eb1fa1e0cddb9994aa0032da687c0
Kit La Touche <kit@transneptune.net>
2016-08-01 23:49:17 -0600

    View details in execute view.

--- app/controllers/execute.js
+++ app/controllers/execute.js
@@ -5,6 +5,11 @@ export default Ember.Controller.extend({
     complete (task) {
       task.set('completed', true);
       task.save();
+    },
+
+    focusTask (task) {
+      const focused = task.get('focused');
+      task.set('focused', !focused);
     }
   }
 });

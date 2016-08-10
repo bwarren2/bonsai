6f3d17129bf07509ec8f0995c9203405252ed986
Kit La Touche <kit@transneptune.net>
2016-08-10 13:07:31 -0600

    Get deck off of application parent controller.

--- app/mixins/filtered-tasks.js
+++ app/mixins/filtered-tasks.js
@@ -2,5 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Mixin.create({
   application: Ember.inject.controller(),
+  decks: Ember.computed.alias('application.decks'),
   filteredTasks: Ember.computed.alias('application.tasks')
 });

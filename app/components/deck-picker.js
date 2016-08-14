d9dadd254ac2bea547fd694572fb442adb778076
Kit La Touche <kit@transneptune.net>
2016-08-13 23:14:10 -0600

    Shrink deck-picker on blur.

--- app/components/deck-picker.js
+++ app/components/deck-picker.js
@@ -4,6 +4,21 @@ export default Ember.Component.extend({
   showAddDeck: false,
   deckTitle: '',
 
+  hideAddDeck: Ember.on('focusOut', function () {
+    if (this.get('showAddDeck')) {
+      Ember.run.later(this, () => {
+        const focussedElement = document.activeElement;
+        const isFocussedOut = (
+          this.$().has(focussedElement).length === 0 && !this.$().is(focussedElement)
+        );
+
+        if (isFocussedOut) {
+          this.set('showAddDeck', false);
+        }
+      }, 0);
+    }
+  }),
+
   actions: {
     setActiveDeck (deck) {
       this.sendAction("setActiveDeck", deck);

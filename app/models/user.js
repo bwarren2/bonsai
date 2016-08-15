c9907c2fd8de461236da535710563ea94613b2a5
Kit La Touche <kit@transneptune.net>
2016-08-15 14:01:13 -0600

    Show/hide help based on user preference.

--- app/models/user.js
+++ app/models/user.js
@@ -4,5 +4,6 @@ import attr from 'ember-data/attr';
 export default Model.extend({
   username: attr('string'),
   display_name: attr('string'),
-  enable_keyboard_shortcuts: attr('boolean')
+  enable_keyboard_shortcuts: attr('boolean'),
+  show_help: attr('boolean')
 });

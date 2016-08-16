31c6eb8e65411bf74737604e179ece7f874576cd
Kit La Touche <kit@transneptune.net>
2016-08-16 17:50:30 -0600

    Make sourcemaps in production to upload to Sentry.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,11 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     'ember-bootstrap': {
-      'importBootstrapTheme': true
+      importBootstrapTheme: true
+    },
+    sourcemaps: {
+      enabled: true,
+      extensions: ['js']
     }
   });
 

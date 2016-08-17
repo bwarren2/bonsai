79a06f6659177838ea41c97352d499d2400affad
Ben Warren <bwarren2@gmail.com>
2016-08-17 14:55:38 -0600

    Example animationness
    WIP
    
    Workable test
    
    WIP
    
    WTF no d3?
    
    Animation demo

--- app/controllers/execute.js
+++ app/controllers/execute.js
@@ -1,3 +1,4 @@
+// from d3 import 'd3';
 import Ember from 'ember';
 import FilteredTasks from '../mixins/filtered-tasks';
 
@@ -5,11 +6,34 @@ export default Ember.Controller.extend(FilteredTasks, {
   actions: {
     complete (task) {
       task.set('pendingCompletion', true);
+      var destination = {
+        top:0,
+        left:200,
+      };
+
+      var jq_select = Ember.$('div.task-card').filter(function() {
+        return Ember.$(this).data("task-id") == task.get("id");
+      });
+      var width = jq_select.width();
+
+      console.log(width);
+      d3.select(jq_select.get(0))
+      .style('position', 'absolute')
+      .style('width', width+"px")
+      .transition()
+      .duration(2000)
+      .style('width', 300+"px")
+      .style('left', destination.left+"px")
+      .style('top', destination.top+"px")
+      .transition()
+      .duration(100000)
+
       // Run later to allow animation to complete.
+      // this.$("#test").fadeOut("slow");
       Ember.run.later(this, () => {
         task.set('pendingCompletion', false);
         task.complete();
-      }, 500);
+      }, 10000);
     },
 
     focusTask (task) {

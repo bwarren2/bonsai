7580e61a03479dbfa4a761632f88c46803dcdb22
Kit La Touche <kit@transneptune.net>
2016-09-16 14:51:36 -0600

    Clean up unused code

--- app/mixins/authenticated-data.js
+++ app/mixins/authenticated-data.js
@@ -2,15 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Mixin.create({
   model () {
-    const deckParams = {
-      deleted_at: 'null'
-    };
-    const taskParams = {};
     if (this.get('session.isAuthenticated')) {
       return Ember.RSVP.hash({
         currentUser: this.get('session.currentUser'),
-        decks: this.store.findAll('deck'), // query('deck', deckParams),
-        tasks: this.store.findAll('task') // query('task', taskParams)
+        decks: this.store.findAll('deck'),
+        tasks: this.store.findAll('task')
       });
     } else {
       return Ember.RSVP.hash({
